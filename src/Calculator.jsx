127bad3b46616f6944cbf494b7f06ee0f00dc94b
kamaleshKDK <kamaleshkdk@gmail.com>
2022-08-06 21:49:25 +0530

    css added

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import "./style.css"
 
 function Calculator() {
     const [number1, setNumber1] = useState();
     const [number2, setNumber2] = useState();
     const [operation, setOperation] = useState("add");
-    const [total, setTotal] = useState(0);
+    const [total, setTotal] = useState();
 
     const calculating = () => {
         if (operation == "add") {
@@ -27,35 +28,38 @@ function Calculator() {
     }
     return (
         <>
-            <h1>Calculator</h1>
-            <div>
-                <label>Number 1 :
+            <h1 className='title'>CALCULATOR</h1>
+            <div className='container'>
+                
                     <input
                         type="number"
                         value={number1}
                         placeholder="Enter a Number"
                         onChange={(e) => setNumber1(e.target.value)}
+                        required
                     />
-                </label>
-
-                <label>Number 2 :
+              
+                    <br/>
                     <input
                         type="number"
                         value={number2}
                         placeholder="Enter a Number"
                         onChange={(e) => setNumber2(e.target.value)}
+                        required
                     />
-                </label>
-                <select>
-                    <option onChang={() => setOperation("add")} value="add">Add</option>
-                    <option onChang={() => setOperation("sub")} value="sub">Sub</option>
-                    <option onChang={() => setOperation("mul")} value="mul">Mul</option>
-                    <option onChang={() => setOperation("div")} value="div">Div</option>
+                
+                <br/>
+                <select className='select'>
+                    <option onChang={() => setOperation("add")} value="add">Add ( + )</option>
+                    <option onChang={() => setOperation("sub")} value="sub">Substract ( - )</option>
+                    <option onChang={() => setOperation("mul")} value="mul">Multiple ( * )</option>
+                    <option onChang={() => setOperation("div")} value="div">Division ( / )</option>
                 </select>
                 <br></br>
-                <button onClick={calculating}>Calculate</button>
-                <button onClick={Clear}>Clear</button>
-                <h3 >Total : {total}</h3>
+                <button onClick={calculating} className="calculate">Calculate</button>
+                <button onClick={Clear} className="btn">Clear</button>
+                {/* <h3 className='total'>Total : {total}</h3> */}
+                <input className='total' value={total} placeholder="Total" />
             </div>
         </>
     )
